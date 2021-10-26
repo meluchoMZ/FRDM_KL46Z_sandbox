5de1cc3ad6d6396fc1315c274a1bebd1a7b2e207
Emilio J. Padrón González <emilioj@udc.gal>
2021-10-26 17:47:57 +0200

    Alternatively blinking both LEDs

--- main.c
+++ main.c
@@ -41,13 +41,32 @@ void led_red_toggle(void)
   GPIOE->PTOR = (1 << 29);
 }
 
+// LED_RED = PTE29
+// LED_GREEN = PTD5
+void leds_init()
+{
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK | SIM_SCGC5_PORTE_MASK;
+  PORTD->PCR[5] = PORT_PCR_MUX(1);
+  PORTE->PCR[29] = PORT_PCR_MUX(1);
+  GPIOD->PDDR |= (1 << 5);
+  GPIOE->PDDR |= (1 << 29);
+  // both LEDS off after init
+  GPIOD->PSOR = (1 << 5);
+  GPIOE->PSOR = (1 << 29);
+}
+
 int main(void)
 {
-  led_green_init();
+  leds_init();
 
   while (1) {
-    led_green_toggle();
+    led_green_toggle(); // Green ON
     delay();
+    led_green_toggle(); // Green OFF
+    led_red_toggle();   // Red ON
+    delay();
+    led_red_toggle();   // Red OFF
   }
 
   return 0;

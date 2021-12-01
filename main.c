29dd9553c3f0f30cb1c1e4b81da46acec96bea1c
Miguel Blanco <miguelgodon@outlook.es>
2021-12-01 18:11:28 +0100

    p5 base code (lab3 code)

--- main.c
+++ main.c
@@ -11,6 +11,21 @@
 
 // Enable IRCLK (Internal Reference Clock)
 // see Chapter 24 in MCU doc
+#define GREEN 0
+#define RED 1
+#define UNDEFINED 255
+#define LOCKED 0
+#define UNLOCKED 1
+
+
+// vars
+volatile static uint8_t lock = LOCKED;
+volatile static uint8_t hits = 0x0;
+volatile static uint8_t misses = 0x0;
+volatile static uint8_t ledStatus = UNDEFINED;
+
+
+
 void irclk_ini()
 {
   MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
@@ -29,7 +44,10 @@ void sw1_ini()
 {
   SIM->COPC = 0;
   SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+  PORTC->PCR[3] |= PORT_PCR_MUX(1);
+  PORTC->PCR[3] |= (1 << 16);
+  PORTC->PCR[3] |= (1 << 19);
+  PORTC->PCR[3] |= 0x3;
   GPIOC->PDDR &= ~(1 << 3);
 }
 
@@ -38,7 +56,10 @@ void sw2_ini()
 {
   SIM->COPC = 0;
   SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[12] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+  PORTC->PCR[12] |= PORT_PCR_MUX(1);
+  PORTC->PCR[12] |= (1 << 16);
+  PORTC->PCR[12] |= (1 << 19);
+  PORTC->PCR[12] |= 0x3;
   GPIOC->PDDR &= ~(1 << 12);
 }
 
@@ -112,34 +133,99 @@ void leds_ini()
 // - Left switch matches red light
 // - Right switch matches green light
 
+void switchLeds (int green)
+{
+	if (green == 0)	{
+		GPIOD->PCOR |= (1 << 5);
+		GPIOE->PSOR |= (1 << 29);
+		ledStatus = GREEN;
+	} else {
+		GPIOD->PSOR |= (1 << 5);
+		GPIOE->PCOR |= (1 << 29);
+		ledStatus = RED;
+	}
+}
+
+void enableInterrupts(void)
+{
+	NVIC_SetPriority(31,1);
+	NVIC_EnableIRQ(31);
+}
+
+void disableInterrupts(void)
+{
+	NVIC_DisableIRQ(31);
+}
+	
+void PORTDIntHandler(void)
+{
+	if ((PORTC->PCR[3] & (1 << 24)) == (1 << 24)) {
+		// interrupt on sw1
+		PORTC->ISFR |= (1 << 3);
+		if (ledStatus == GREEN) {
+			hits++;
+		} else {
+			misses++;
+		}
+	}
+
+	if ((PORTC->PCR[12] & (1 << 24)) == (1 << 24)) {
+		// interrupt on sw2
+		PORTC->ISFR |= (1 << 12);
+		if (ledStatus == RED) {
+			hits++;
+		} else {
+			misses++;
+		}
+	}
+	// unlocking loop so that the "game" can continue
+	lock = UNLOCKED;
+}
+
 int main(void)
 {
+  leds_ini();
+
+  sw1_ini();
+  sw2_ini();
+
   irclk_ini(); // Enable internal ref clk to use by LCD
 
   lcd_ini();
-  lcd_display_dec(666);
+  enableInterrupts();
 
   // 'Random' sequence :-)
   volatile unsigned int sequence = 0x32B14D98,
     index = 0;
 
   while (index < 32) {
+  	lcd_display_time(hits,misses);
+	lock = LOCKED;
     if (sequence & (1 << index)) { //odd
       //
       // Switch on green led
-      // [...]
+	    switchLeds(GREEN);
       //
     } else { //even
       //
       // Switch on red led
-      // [...]
+	    switchLeds(RED);
       //
     }
-    // [...]
+    while (lock == LOCKED)
+    {
+	    ;
+    }
+    index++;
   }
 
   // Stop game and show blinking final result in LCD: hits:misses
-  // [...]
+  lcd_display_time(hits,misses);
+  disableInterrupts();
+  LCD->AR = LCD_AR_BLINK(1);
+  // shutting down leds
+  GPIOD->PSOR = (1 << 5);
+  GPIOE->PSOR |= (1 << 29);
   //
 
   while (1) {

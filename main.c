22eb887fbeeda1a76c07987e3073efba26ffff66
Miguel Blanco <miguelgodon@outlook.es>
2021-12-30 18:17:41 +0100

    TT: Digital metronome

--- main.c
+++ main.c
@@ -1,263 +1,207 @@
-// Embedded systems, 2021
+// Digital metronome 
+// Embedded Systems, 2021
 // Miguel Blanco Godón
-
 
 #include "MKL46Z4.h"
 #include "lcd.h"
 
-#define GREEN 0
-#define RED 1
-#define INVALID 0
-#define VALID 1
-#define DOT 0
-#define DASH 1
-#define S 5
-#define O 0
-#define NONE 255
-#define SO 50
-#define SOS 55
 #define TRUE 0
 #define FALSE 1
-#define TIMEOUT_DEFAULT 10000
+#define TIMEOUT 1000
+#define REF 60000 // 60*1000
 
+volatile static uint8_t speed = 60, mustTick = FALSE;
+volatile static unsigned long timeout = TIMEOUT;
 
-
-volatile static uint8_t isSOS = FALSE;
-volatile static uint8_t arDefault;
-volatile static long timeout = TIMEOUT_DEFAULT;
-
-void irclk_ini()
+void init_green_led(void)
 {
-  MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
-  MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
+	SIM->COPC = 0x0;
+	SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	PORTD->PCR[5] = PORT_PCR_MUX(1);
+	GPIOD->PDDR |= (1 << 5);
+	GPIOD->PSOR = (1 << 5);
 }
 
-void sw1_ini()
+void init_red_led(void)
 {
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[3] |= PORT_PCR_MUX(1);
-  PORTC->PCR[3] |= (1 << 16);
-  PORTC->PCR[3] |= (1 << 19);
-  PORTC->PCR[3] |= 0x3;
-  GPIOC->PDDR &= ~(1 << 3);
+	SIM->COPC = 0x0;
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	PORTE->PCR[29] = PORT_PCR_MUX(1);
+	GPIOE->PDDR |= (1 << 29);
+	GPIOE->PSOR = (1 << 29);
 }
 
-void sw2_ini()
+void enable_sw3(void)
 {
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[12] |= PORT_PCR_MUX(1);
-  PORTC->PCR[12] |= (1 << 16);
-  PORTC->PCR[12] |= (1 << 19);
-  PORTC->PCR[12] |= 0x3;
-  GPIOC->PDDR &= ~(1 << 12);
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+	PORTC->PCR[12] = PORT_PCR_MUX(1);
+	PORTC->PCR[12] |= (1 << 16);
+	PORTC->PCR[12] |= (1 << 19);
+	PORTC->PCR[12] |= 0x3;
+	GPIOC->PDDR &= ~(1 << 12);
 }
 
-void sws_ini()
+void enable_sw1(void)
 {
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
-  PORTC->PCR[12] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
-  GPIOC->PDDR &= ~(1 << 3 | 1 << 12);
+	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+	PORTC->PCR[3] = PORT_PCR_MUX(1);
+	PORTC->PCR[3] |= (1 << 16);
+	PORTC->PCR[3] |= (1 << 19);
+	PORTC->PCR[3] |= 0x3;
+	GPIOC->PDDR &= ~(1 << 3);
 }
 
-void led_green_ini()
+void enable_gpio_interrupt(void)
 {
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
-  PORTD->PCR[5] = PORT_PCR_MUX(1);
-  GPIOD->PDDR |= (1 << 5);
-  GPIOD->PSOR = (1 << 5);
-}
-
-void led_green_toggle()
-{
-  GPIOD->PTOR = (1 << 5);
-}
-
-void led_red_ini()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-  PORTE->PCR[29] = PORT_PCR_MUX(1);
-  GPIOE->PDDR |= (1 << 29);
-  GPIOE->PSOR = (1 << 29);
-}
-
-void led_red_toggle(void)
-{
-  GPIOE->PTOR = (1 << 29);
-}
-
-void leds_ini()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK | SIM_SCGC5_PORTE_MASK;
-  PORTD->PCR[5] = PORT_PCR_MUX(1);
-  PORTE->PCR[29] = PORT_PCR_MUX(1);
-  GPIOD->PDDR |= (1 << 5);
-  GPIOE->PDDR |= (1 << 29);
-  // both LEDS off after init
-  GPIOD->PSOR = (1 << 5);
-  GPIOE->PSOR = (1 << 29);
-}
-
-
-void enableInterrupts(void)
-{
-	NVIC_SetPriority(31,1);
+	// enables and set priority for button irq
+	NVIC_SetPriority(31, 1);
 	NVIC_EnableIRQ(31);
 }
 
-void disableInterrupts(void)
+
+void display(void)
 {
+	// cleans lcd
+	LCD->AR = LCD_AR_BLANK_MASK;
+	// restarts lcd
+	lcd_ini();
+	lcd_display_dec(speed);
+}
+void PORTDIntHandler(void)
+{
+	// disables IRQ (software button debouncing)
 	NVIC_DisableIRQ(31);
+	// checks which button was pressed
+	// updates speed consequently
+	if ((PORTD->PCR[4] & (1 << 24)) == (1 << 24)) {
+		PORTD->ISFR |= (1 << 4);
+		speed--;
+	}
+	if ((PORTD->PCR[5] & (1 << 24)) == (1 << 24)) {
+		PORTD->ISFR |= (1 << 5);
+		speed++;
+	}
+	if ((PORTD->PCR[6] & (1 << 24)) == (1 << 24)) {
+		PORTD->ISFR |= (1 << 6);
+		speed -= 10;
+	}
+	if ((PORTD->PCR[7] & (1 << 24)) == (1 << 24)) {
+		PORTD->ISFR |= (1 << 7);
+		speed += 10;
+	}
+	// establishes speed upper and lower boundaries
+	if (speed > 240) 
+		speed = 240;
+	if (speed < 30) 
+		speed = 30;
+
+	// updates display
+	display();
+
+
+	// button pressed feedback 
+	GPIOE->PCOR |= (1 << 29);
+	for (int i = 1; i < 250000; i++);
+	GPIOE->PSOR |= (1 << 29);
+	// assures there is not any active button IRQ
+	PORTD->ISFR |= (1 << 4);
+	PORTD->ISFR |= (1 << 5);
+	PORTD->ISFR |= (1 << 6);
+	PORTD->ISFR |= (1 << 7);
+	// reenables IRQ
+	NVIC_EnableIRQ(31);
+}
+
+void enableButtons(void)
+{
+	SIM->COPC = 0x0;
+	SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
+	// PTD5
+	PORTD->PCR[5] = PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+	PORTD->PCR[5] |= (1 << 16);
+	PORTD->PCR[5] |= (1 << 19);
+	PORTD->PCR[5] |= 0x3;
+	GPIOD->PDDR &= ~(1 << 5);
+
+	// PTD7
+	PORTD->PCR[7] = PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+	PORTD->PCR[7] |= (1 << 16);
+	PORTD->PCR[7] |= (1 << 19);
+	PORTD->PCR[7] |= 0x3;
+	GPIOD->PDDR &= ~(1 << 7);
+	
+	// PTD6
+	PORTD->PCR[6] = PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+	PORTD->PCR[6] |= (1 << 16);
+	PORTD->PCR[6] |= (1 << 19);
+	PORTD->PCR[6] |= 0x3;
+	GPIOD->PDDR &= ~(1 << 6);
+
+	// PTl4
+	PORTD->PCR[4] = PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+	PORTD->PCR[4] |= (1 << 16);
+	PORTD->PCR[4] |= (1 << 19);
+	PORTD->PCR[4] |= 0x3;
+	GPIOD->PDDR &= ~(1 << 4);
+}
+
+void enableOutput(void)
+{
+	// PTE20 -> BUZZER & LED
+	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	PORTE->PCR[20] = PORT_PCR_MUX(1);
+	GPIOE->PDDR |= (1 << 20);
+	GPIOE->PCOR |= (1 << 20);
+}
+
+
+void irclk_ini()
+{
+	MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
+	MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
 }
 
 void SysTickIntHandler(void)
 {
 	if (timeout == TRUE) {
-		GPIOE->PCOR |= (1 << 29);
-		for (int i = 0; i < 100000; i++);
-		GPIOE->PSOR |= (1 << 29);
-		// request system request
-		SCB->AIRCR = (0x5FA<<SCB_AIRCR_VECTKEY_Pos)|SCB_AIRCR_SYSRESETREQ_Msk;
+		timeout = REF/speed;
+		// sets flag to ring the buzzer
+		mustTick = TRUE;
 	} else {
 		timeout--;
 	}
 }
 
-
-void PORTDIntHandler(void)
+void configureSystemClock(void)
 {
-	uint8_t validInput = INVALID;
-	uint8_t input;
-	volatile static uint8_t state;
-	uint8_t nextState = 0x00;
-	volatile static uint8_t lcdValue = NONE;
-
-	timeout = TIMEOUT_DEFAULT; // resets "watchdog" timer
-
-	if ((PORTC->PCR[3] & (1 << 24)) == (1 << 24)) {
-		PORTC->ISFR |= (1 << 3);
-		validInput = VALID;
-		input = DOT;
-		GPIOD->PCOR |= (1 << 5);
-		for (int i = 0; i < 100000; i++);
-		GPIOD->PSOR |= (1 << 5);
-	}
-
-	if ((PORTC->PCR[12] & (1 << 24)) == (1 << 24)) {
-		PORTC->ISFR |= (1 << 12);
-		validInput = (validInput == VALID) ? INVALID : VALID;
-		input = DASH;
-		GPIOD->PCOR |= (1 << 5);
-		for (int i = 0; i < 300000; i++);
-		GPIOD->PSOR |= (1 << 5);
-	}
-
-	if (validInput == INVALID) {
-		return;
-	}
-	switch (state)
-	{
-		// S(0)
-		case 0x01:
-			nextState = (input == DOT) ? 0x02 : 0x00;
-			if (input == DASH)
-				lcdValue = NONE;
-			break;
-		// S(1)
-		case 0x02:
-			nextState = 0x00;
-			if (input == DASH) {
-				lcdValue = NONE;
-				break;
-			}
-			if (lcdValue == SO) {
-				lcdValue = SOS;
-				break;
-			}
-			lcdValue = S;
-			break;
-		// O(0)
-		case 0x04:
-			nextState = (input == DASH) ? 0x05 : 0x00;
-			if (input == DOT || lcdValue == SO)
-				lcdValue = NONE;
-			break;
-		// O(1)
-		case 0x05:
-			nextState = 0x00; 
-			if (input == DOT) {
-				lcdValue = NONE;
-				break;
-			}
-			lcdValue = (lcdValue == S) ? SO : NONE;
-			break;
-		// initial state
-		default:
-			nextState = (input == DOT) ? 0x01 : 0x04;
-			if (lcdValue == SO && input == DASH)
-				lcdValue = NONE;
-			if (lcdValue == S && input == DOT)
-				lcdValue = NONE;
-			break;
-	}
-	switch (lcdValue)
-	{
-		case S:
-  			//LCD->AR = LCD_AR_BLANK_MASK; 
-			//LCD->AR = arDefault;
-			lcd_ini();
-			lcd_set(S, 4);
-			break;
-		case SO:
-  			//LCD->AR = LCD_AR_BLANK_MASK; 
-			//LCD->AR = arDefault;
-			lcd_ini();
-			lcd_set(S, 3);
-			lcd_set(O, 4);
-			break;
-		case SOS:
-  			//LCD->AR = LCD_AR_BLANK_MASK; 
-			//LCD->AR = arDefault;
-			lcd_ini();
-			lcd_set(S, 2);
-			lcd_set(O, 3);
-			lcd_set(S, 4);
-			isSOS = TRUE;
-			break;
-		default:
-  			LCD->AR = LCD_AR_BLANK_MASK; 
-			break;
-	}
-	state = nextState;
+	SysTick->CTRL = 0;
+	// 48 kHz tick
+	SysTick->LOAD = (DEFAULT_SYSTEM_CLOCK/1000) - 1;
+	SysTick->VAL = 0;
+	SysTick->CTRL = 0x7;
 }
 
 int main(void)
 {
-  leds_ini();
-
-  sw1_ini();
-  sw2_ini();
-
-  irclk_ini(); // Enable internal ref clk to use by LCD
-
-  lcd_ini();
-  arDefault = LCD->AR;
-  enableInterrupts();
-
-  SysTick_Config(0x2000U);
-
-  while (isSOS == FALSE) 
-	  ;
-  
-  GPIOD->PSOR |= (1 << 5);
-  GPIOE->PCOR |= (1 << 29);
-  disableInterrupts();
-  LCD->AR = LCD_AR_BLINK(1);
-  return 0;
+	configureSystemClock();
+	init_green_led();
+	init_red_led();
+	irclk_ini();
+	lcd_ini();
+	display();
+	enableButtons();
+	enableOutput();
+	enable_gpio_interrupt();
+	
+	while (1) {
+		if (mustTick == TRUE) {
+			// writes to connected buzzer & led
+			GPIOE->PSOR |= (1 << 20);
+			for (int i = 0; i < 100000; i++);
+			GPIOE->PCOR |= (1 << 20);
+			mustTick = FALSE;
+		} 
+	}
+	return 0;
 }
+
